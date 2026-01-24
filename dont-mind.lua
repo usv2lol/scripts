573a143ce2f25c80e009966322f46a2c3fee26fa
usv2 <veyra123lol@gmail.com>
2026-01-24 19:09:24 +0100

    Add teleport queue on game close

--- dont-mind.lua
+++ dont-mind.lua
@@ -1,3 +1,7 @@
+queueonteleport([[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/usv2lol/scripts/refs/heads/main/dont-mind.lua"))()
+]])
+
 game.Close:Connect(function()
 messagebox("You Left " .. game.PlaceId, game.JobId, 0)
 end)
